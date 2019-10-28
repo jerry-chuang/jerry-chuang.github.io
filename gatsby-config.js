739002d1f42036fbf6d0e94f775851446c23de79
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-28 15:30:26 -0700

    Update site meta data and prismic data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Prist | Gatsby & Prismic Starter`,
-        description: `A starter powered by Gatsby and Prismic to showcase portfolios and blogs.`,
-    author: `Marguerite Roth | marguerite.io`,
+    title: `Jerry Chuang's Portfolio`,
+        description: `A portfolio powered by Gatsby and Prismic to showcase portfolios and blogs.`,
+    author: `Jerry Chuang`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,7 +26,7 @@ module.exports = {
     {
         resolve: 'gatsby-source-prismic-graphql',
         options: {
-            repositoryName: 'prist', // (REQUIRED, replace with your own)
+            repositoryName: 'JerChuang', // (REQUIRED, replace with your own)
             linkResolver: () => post => `/${post.uid}`,
         }
     },
