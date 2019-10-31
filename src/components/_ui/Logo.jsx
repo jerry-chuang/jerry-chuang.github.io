3f2dffcb73e2a5fd43496ff4886f75b8b75d0816
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-31 11:21:03 -0700

    Fix syntax error

--- src/components/_ui/Logo.jsx
+++ src/components/_ui/Logo.jsx
@@ -7,13 +7,13 @@ const Logo = () => (
   <title>background</title>
   <rect fill="none" id="canvas_background" height="72" width="72" y="-1" x="-1"/>
   <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
-   <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+   <rect fill="url(#gridpattern)" strokeWidth="0" y="0" x="0" height="100%" width="100%"/>
   </g>
  </g>
  <g>
   <title>Layer 1</title>
-  <circle stroke="#000"  id="svg_8" cy="35" cx="35" r="35" stroke-width="0" fill="#0080FF"/>
-  <text transform="matrix(1,0,0,1,0,0) " font-style="normal" font-weight="bold" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="40" id="svg_7" y="55" x="15" stroke-width="0" stroke="#000" fill="#fff">JC</text>
+  <circle stroke="#000"  id="svg_8" cy="35" cx="35" r="35" strokeWidth="0" fill="#0080FF"/>
+  <text transform="matrix(1,0,0,1,0,0) " fontStyle="normal" fontWeight="bold" textAnchor="start" fontFamily="Helvetica, Arial, sans-serif" fontSize="40" id="svg_7" y="55" x="15" strokeWidth="0" stroke="#000" fill="#fff">JC</text>
  </g>
 </svg>
 )
